d68fb2988b429d2ac91524dac5ad59d4792ae52b
Hassan Bazzi <me@hassanb.com>
2021-08-31 22:00:53 -0400

    update theme

--- index.php
+++ index.php
@@ -138,8 +138,8 @@ if (file_exists('LookingGlass/Config.php')) {
           <div class="well">
             <span id="legend">Network information</span><!-- IE/Safari dislike <legend> out of context -->
             <p>Server Location: <b><?php echo $serverLocation; ?></b></p>
-            <div style="margin-left: 10px;">
-              <p>Test IPv4: <?php echo $ipv4; ?></p>
+            <div style="margin-left: 10px;" class="font-mono">
+              <p>Test IPv4: <span><?php echo $ipv4; ?></span></p>
               <p><?php if (!empty($ipv6)) {
                     echo 'Test IPv6: ', $ipv6;
                   } ?></p>
