02547d757711703fceb16c53faf2213d13700bc2
Hassan Bazzi <me@hassanb.com>
2021-09-02 18:29:51 -0400

    update theme

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (file_exists('LookingGlass/Config.php')) {
 
 <head>
   <meta charset="utf-8">
-  <title><?php echo $siteName; ?> - Looking Glass</title>
+  <title><?php echo $serverLocation; ?> - Looking Glass</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="LookingGlass - Open source PHP looking glass">
   <meta name="author" content="Telephone">
